daee52cd0e263b97c691f17afbd2e6e3517e59e0
Joshua Saunders <jk.saunders@hotmail.com>
2019-07-07 09:10:36 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Button } from "@jksaunders/bonfire";
+
+const App = () => (
+  <Button>Hi!</Button>  
+);
+
+ReactDOM.render(
+	<App />,
+	document.getElementById("root")
+);
