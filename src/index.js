bc3884870cf173cd3923252c46512e0ecd2fa2f9
Joshua Saunders <jk.saunders@hotmail.com>
2019-07-10 09:58:55 -0400

    Added HtmlWebpackPlugin

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ThemeProvider } from "styled-components";
 import { Button } from "@jksaunders/bonfire";
 
 const App = () => (
-  <Button>Hello!</Button>  
+  <Button variant={Button.CONSTANTS.VARIANT.PRIMARY}>Hello!</Button>
 );
 
 ReactDOM.render(
-	<App />,
-	document.getElementById("root")
+  (
+    <ThemeProvider theme={{ mode: "dark", layout: "compact" }}>
+      <App />
+    </ThemeProvider>
+  ),
+  // eslint-disable-next-line no-undef
+  document.getElementById("root")
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -6,6 +7,12 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist")
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "React App Root",
+      template: "index.html"
+    })
+  ],
   module: {
     rules: [
       {
