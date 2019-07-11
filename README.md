d410240d6f28a2c34708c68eddfe47985e8b40a5
Joshua Saunders <jk.saunders@hotmail.com>
2019-07-11 12:35:05 -0400

    Added webpack-dev-server config and build-prod script

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # debra-dynes
+
+To start, run `npm start`. The project will be available at http://localhost:9101.
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,34 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
+module.exports = () => ({
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist")
   },
+  devServer: {
+    contentBase: "./dist",
+    port: 9101
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "React App Root",
+      title: "Debra Dynes Family House",
       template: "index.html"
     })
   ],
   module: {
     rules: [
+      {
+        test: /\.(js|jsx)$/,
+        enforce: "pre",
+        exclude: /node_modules/,
+        loader: "eslint-loader",
+        options: {
+          emitWarnings: true,
+          configFile: "./.eslintrc"
+        }
+      },
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
@@ -24,4 +38,4 @@ module.exports = {
       }
     ]
   }
-};
+});
