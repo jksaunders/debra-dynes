4760e01810c48a79d916a968a443e79116de5626
Joshua Saunders <jk.saunders@hotmail.com>
2019-07-13 19:27:35 -0400

    Added react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,16 +1,50 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
-import { Button } from "@jksaunders/bonfire";
+
+const Home = () => (
+  <h1>Home!</h1>
+);
+
+const AboutUs = () => (
+  <h1>About us!</h1>
+);
+
+const AppRouter = ({ children }) => (
+  <Router>
+    {children}
+    <Route path="/home/" component={Home} />
+    <Route path="/about/" component={AboutUs} />
+  </Router>
+);
+AppRouter.propTypes = {
+  children: PropTypes.node
+};
+AppRouter.defaultProps = {
+  children: null
+};
+
+const Header = () => (
+  <div>
+    <Link to="/home">Home</Link>
+    <Link to="/about">About Us</Link>
+  </div>
+);
 
 const App = () => (
-  <Button variant={Button.CONSTANTS.VARIANT.PRIMARY}>Hello!</Button>
+  <div>
+    <Header />
+  </div>
 );
 
 ReactDOM.render(
   (
     <ThemeProvider theme={{ mode: "dark", layout: "compact" }}>
-      <App />
+      <AppRouter>
+        <App />
+      </AppRouter>
     </ThemeProvider>
   ),
   // eslint-disable-next-line no-undef
