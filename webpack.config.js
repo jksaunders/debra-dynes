6cf3fe0b3d190258f19ad061fa7a1e052b5807ff
Joshua Saunders <jk.saunders@hotmail.com>
2020-01-19 01:14:47 -0500

    Webpack prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,58 @@
-const path = require("path");
-const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = env => ({
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist")
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   resolve: {
     alias: {
-      react: path.resolve(__dirname, "node_modules", "react"),
-      "styled-components": path.resolve(__dirname, "node_modules", "styled-components"),
-    }
+      react: path.resolve(__dirname, 'node_modules', 'react'),
+      'styled-components': path.resolve(
+        __dirname,
+        'node_modules',
+        'styled-components'
+      ),
+    },
   },
   devServer: {
-    contentBase: "./dist",
+    contentBase: './dist',
     historyApiFallback: true,
-    port: 9101
+    port: 9101,
   },
   plugins: [
     new CleanWebpackPlugin(),
     new webpack.DefinePlugin({
-      "process.env.rootLocation": env ? `"/${env.rootLocation}"` : "\"\""
+      'process.env.rootLocation': env ? `"/${env.rootLocation}"` : '""',
     }),
     new HtmlWebpackPlugin({
-      title: "Debra Dynes Family House",
-      template: "index.html"
-    })
+      title: 'Debra Dynes Family House',
+      template: 'index.html',
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        enforce: "pre",
+        enforce: 'pre',
         exclude: /node_modules|bonfire/,
-        loader: "eslint-loader",
+        loader: 'eslint-loader',
         options: {
           emitWarnings: true,
-          configFile: "./.eslintrc"
-        }
+          configFile: './.eslintrc',
+        },
       },
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
-      }
-    ]
-  }
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
 });
